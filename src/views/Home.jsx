598bb97caa072b45779558f5e5e1284ee5df343f
Crostons <shubh1608003@gmail.com>
2022-09-08 23:10:21 +0530

    Update Home.jsx

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -10,13 +10,16 @@ const Home = () => {
       <img className="object-cover object-center rounded" alt="hero" src={heroImg}/>
     </div>
     <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
-      <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Before they sold out
-        <br className="hidden lg:inline-block"/>readymade gluten
+      <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Fluency On-chain
       </h1>
-      <p className="mb-8 leading-relaxed">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.</p>
+      <p className="mb-8 leading-relaxed">No more hassle to perform operations at multiple cryptos, use clustors and manage your portfolio with a click.</p>
       <div className="flex justify-center">
-        <button className="inline-flex text-white bg-cex border-0 py-2 px-6 focus:outline-none hover:bg-rex rounded text-lg">Button</button>
-        <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">Button</button>
+        <button className="inline-flex text-white bg-cex border-0 py-2 px-6 focus:outline-none hover:bg-rex rounded text-lg">
+          <a href="https://only-patel.gitbook.io/clustors-1/"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-4 h-4 mt-1.5 mr-1">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m6.75 12H9m1.5-12H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
+</svg></a>
+<a href="https://only-patel.gitbook.io/clustors-1/">Docs</a>
+        </button>
       </div>
     </div>
   </div>
